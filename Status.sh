0c7d6a8cf8fa2d27c9380d10250a389bead6e117
diegobnh <37424347+diegobnh@users.noreply.github.com>
2018-11-13 19:18:26 -0300

    Add

---
+++ Status.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+PID=$1 
+while true; 
+do
+    if [ -d "/proc/$PID/task/" ]; then
+       result=$(ps -p $PID -L  -o pid=,tid=,psr=,pcpu=,stat= | paste -d:  -s)
+       time=$(date +"%H:%M:%S:%N")
+       if [[ $? != 1 ]]; then
+           echo -n $time " " $result " " >> Stat.log ;          
+       fi
+    else
+       break ;
+    fi
+    sleep 0.14
+done 
