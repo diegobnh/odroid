aa53a373ee0eeee9622f56344bd9fcce316bbbc9
diegobnh <37424347+diegobnh@users.noreply.github.com>
2019-07-12 19:56:09 -0300

    adaptation for pmus per cluster

--- scheduler/loadAgent.py
+++ scheduler/loadAgent.py
@@ -11,67 +11,19 @@ from stable_baselines.common import set_global_seeds
 import sys
 
 num_actions=3
-index_state=6
-index_exec_time=7
+total_observation_space = 18
+index_state= total_observation_space - 2
+index_exec_time= total_observation_space - 1
 
 map_action_state = [3, 7, 23]
 
 ref_arquivo = open("Output.txt","w", buffering=1)
 
 
-'''
-* All environments should inherit from gym.Env
-
-* At a minimum you must override a handful of methods:
-	step
-	reset
-
-* At a minimum you must provide the following attributes(Both are instances of gym.spaces classes)
-	action_space
-	observation_space
-
-
-	self.action_space = MultiDiscrete([23,10]) //PPO2 - Actions between [0-22, 0-9]
-        self.action_space = MultiDiscrete([23,10]) //DQN - AssertionError: Error: the action space for DQN must be of type gym.spaces.Discrete Não suporta MultiDiscrete!!!
-'''
-
-
-class EnviromentExample(Env):
-    '''
-     Observation:
-        Type: Box(6)
-        Num	Observation
-        0	pmu1
-        1	pmu2
-        2	pmu3
-        3       pmu4
-        4       cpu usage little
-        5       cpu usage big
-        6       state
-        7       exec_time
-
-
-    Actions:
-        Type: Discrete(2)
-        Num	Action
-        0	Setup 4l cores
-        1	Setup 4b cores
-        2	Setup 4b4l cores
-
-    Reward:
-        Reward is 0 for every step taken, except the termination step when receive total reward. In this case o reward total is the execution time
-
-    Episode Termination:
-        When observation_space[4] is equal -1. State equal -1 is an agreement code to sinalize the end of execution.
-        Episode length is greater than some big value
-
-
-    '''
-
-
+class EnviromentExample(Env):    
     def __init__(self):
         self.action_space = Discrete(num_actions)
-        self.observation_space = Box(low=-1, high=100000, shape=(8,))
+        self.observation_space = Box(low=-1, high=100000, shape=(total_observation_space,))
         self.dim = num_actions
         self.acum_reward = 0
         self.immediate_reward = 0
@@ -97,11 +49,9 @@ class EnviromentExample(Env):
         self.num_steps += 1
         self.total_time_steps +=1
 
+        config_action = map_action_state[action]
 
-        a = map_action_state[action]
-
-
-        item = "A"+ str(self.num_steps) + ":" + str(a) 
+        item = "A"+ str(self.num_steps) + ":" + str(config_action) 
         self.action_list.append(item)
 
 
@@ -112,7 +62,8 @@ class EnviromentExample(Env):
            reward = self.get_final_reward()
 
            #ref_arquivo.write(str(self.action_list))
-           ref_arquivo.write("\n" + "Exec_time:" + str(self.state[index_exec_time]) + " Episodio:" + str(self.episodes) +  " Reward:" +  str(reward) + " Steps:" + str(self.num_steps) + " " + str(self.action_list) +  "\n")
+           ref_arquivo.write("\n" + "Exec_time:" + str(self.state[index_exec_time]) + " Episodio:" + str(self.episodes) \
+			     +  " Reward:" +  str(reward) + " Steps:" + str(self.num_steps) + " " + str(self.action_list) +  "\n")
            ref_arquivo.write("\n")
 
            if self.episodes == 1500:
@@ -126,14 +77,22 @@ class EnviromentExample(Env):
 
         else:
            reward = self.get_immediate_reward()
-           ref_arquivo.write(str("{0:.6f}".format(self.state[0])) + "  " + str("{0:.6f}".format(self.state[1])) + "  " + str("{0:.6f}".format(self.state[2])) + "  " + str("{0:.6f}".format(self.state[3])) + "  " + str("{0:.6f}".format(self.state[4])) + "  " + str("{0:.6f}".format(self.state[5]) + "  Acao:" + str(a) + "\n") )
-
-           self.write_to_scheduler(a) #DEPOIS ENVIA UMA A
+	
+	   output=""
+           for value_state in range(total_observation_space):
+                aux1 = "str(\"{0:.2f}\".format(self.state["   
+                aux2 = "])) + \"  \" + "
+                sample = aux1 + str(value_state) + aux2
+                output += sample
+		
+	   output = output[:-6] + " Action:" + str(config_action) + "\\n" + "\""
+           ref_arquivo.write(output) 
+	
+           self.write_to_scheduler(config_action) 
            self.state = self.read_from_scheduler()
 
 
         return self.state, reward, done, {}
-
 
 
     def get_immediate_reward(self):
@@ -176,18 +135,35 @@ class EnviromentExample(Env):
         pass
 
     def read_from_scheduler(self):
-        pmu1_str, pmu2_str, pmu3_str, pmu4_str, cpu_usage_little_str, cpu_usage_big_str, state_str, exec_time_str = input().split() #RECEBIMENTO DO ESTADO DA MAIN
-        pmu1 = float.fromhex(pmu1_str)
-        pmu2 = float.fromhex(pmu2_str)
-        pmu3 = float.fromhex(pmu3_str)
-        pmu4 = float.fromhex(pmu4_str)
+        L_pmu1_str, L_pmu2_str, L_pmu3_str, L_pmu4_str, L_pmu5_str, \
+	B_pmu1_str, B_pmu2_str, B_pmu3_str, B_pmu4_str, B_pmu5_str,B_pmu6_str, B_pmu7_str, \
+	cpu_migration_str, context_switch_str, cpu_usage_little_str, cpu_usage_big_str, state_str, exec_time_str = input().split() #RECEBIMENTO DO ESTADO DA MAIN
+        
+	L_pmu1 = float.fromhex(L_pmu1_str)
+        L_pmu2 = float.fromhex(L_pmu2_str)
+        L_pmu3 = float.fromhex(L_pmu3_str)
+        L_pmu4 = float.fromhex(L_pmu4_str)
+	L_pmu5 = float.fromhex(L_pmu5_str)
+	
+	B_pmu1 = float.fromhex(B_pmu1_str)
+        B_pmu2 = float.fromhex(B_pmu2_str)
+        B_pmu3 = float.fromhex(B_pmu3_str)
+        B_pmu4 = float.fromhex(B_pmu4_str)
+	B_pmu5 = float.fromhex(B_pmu5_str)
+        B_pmu6 = float.fromhex(B_pmu6_str)
+	B_pmu7 = float.fromhex(B_pmu7_str)
+
+        cpu_migration = float.fromhex(cpu_migration_str)
+        context_switch = float.fromhex(context_switch_str)
+
         cpu_usage_little  = float.fromhex(cpu_usage_little_str)
         cpu_usage_big  = float.fromhex(cpu_usage_big_str)
-        current_state = float(state_str)
-        exec_time = float(exec_time_str)
 
-        return np.array([pmu1, pmu2, pmu3, pmu4, cpu_usage_little, cpu_usage_big, current_state, exec_time])
+	return np.array([L_pmu1, L_pmu2, L_pmu3, L_pmu4, L_pm5, \
+			 B_pmu1, B_pmu2, B_pmu3, B_pmu4, B_pmu5,B_pmu6, B_pmu7, \
+			 cpu_migration, context_switch, cpu_usage_little, cpu_usage_big, num_little, num_big, exec_time])
 
+	
     def write_to_scheduler(self, action):
         print (action)
 
